3808b139281f94275e524818f9e424ce33364dff
i0xbean <i0xbean@gmail.com>
2013-06-24 17:18:43 +0800

    update baochuan & 改进细节

---
+++ Pods/Pods-Baochuan-environment.h
@@ -0,0 +1,50 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 0
+
+// CocoaLumberjack
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack
+#define COCOAPODS_VERSION_MAJOR_CocoaLumberjack 1
+#define COCOAPODS_VERSION_MINOR_CocoaLumberjack 6
+#define COCOAPODS_VERSION_PATCH_CocoaLumberjack 2
+
+// EGOCache
+#define COCOAPODS_POD_AVAILABLE_EGOCache
+#define COCOAPODS_VERSION_MAJOR_EGOCache 2
+#define COCOAPODS_VERSION_MINOR_EGOCache 0
+#define COCOAPODS_VERSION_PATCH_EGOCache 0
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 6
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
+
+// Reachability
+#define COCOAPODS_POD_AVAILABLE_Reachability
+#define COCOAPODS_VERSION_MAJOR_Reachability 3
+#define COCOAPODS_VERSION_MINOR_Reachability 1
+#define COCOAPODS_VERSION_PATCH_Reachability 0
+
+// SDWebImage
+#define COCOAPODS_POD_AVAILABLE_SDWebImage
+#define COCOAPODS_VERSION_MAJOR_SDWebImage 2
+#define COCOAPODS_VERSION_MINOR_SDWebImage 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage 4
+
+// SDWebImage/Main
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_Main
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_Main 2
+#define COCOAPODS_VERSION_MINOR_SDWebImage_Main 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage_Main 4
+
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-mini_baixing
-============
+# 『嗨，宝船』
+
+## 作用
+- 快速了解`宝船`项目结构、常用组件
+- 尽量少的代码完成百姓网客户端基本浏览功能
+
+## 功能集（业务）
+- Category
+	- 分级显示一二级类目
+	- 一级类目显示 icon、类目名称
+	- 二级类目显示类目名称
+- Listing
+	- 按时间顺序显示 ads
+	- 可加载更多
+	- 可重新加载
+	- 每个 ad 显示 标题、图片、时间
+- viewad
+	- 显示 标题、`metas`、描述
+	- 可拨打电话
+	- 可查看大图
+
+## 知识点（技术）
+- 项目依赖
+	- Cocoapods
+- 宝船 
+	- data：BCCategory，BCAd
+	- BCViewController
+	- 手势返回
+	- BCConfig
+
+## 实现
+- 后续 _完成 mini-baixing 者_ 有资格 __提议__ 改进 `staging`
